b48b6a9576ca4b695f2c749b9ec6e08fdcf25b94
FarahTSG <farah@thirdspaceglobal.com>
2023-02-25 16:19:07 +0530

    fix: update server url

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
 
   //fetch bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-ai-cwgr.onrender.com/', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
